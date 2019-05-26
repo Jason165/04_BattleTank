f32939a1a693b353afbe336934c47336251cf42c
JovanKostic <jovankostic@outlook.de>
2019-05-26 10:42:26 +0200

    BT20 Tick Method

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -12,14 +12,30 @@ void ATankPlayerController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
 	}
-	else 
+	else
 	{
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing a %s"), *(ControlledTank->GetName()));
 	}
-	
+
 }
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+	
+	// Get world location if linetrace trough crosshair
+	// If it hits the landscape
+			// Tell controlled tank to aim at this point
 }
